512279f3808a1c41d68f2bcc958b68421ae6f439
lCherS <lucascherez.cl@gmail.com>
2022-09-14 10:01:20 -0300

    Acrescentando banco com Prisma

--- server/src/server.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const app = express();
-app.get('/ads', (req, res) => {
-    console.log('oi');
-    return res.send('concluid');
-});
-app.listen(3333);
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,19 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (req, res) => {
-console.log('oi');
-return res.send('concluido')
+app.use(express.json());
+// HTTP methods / API RESTful
+
+app.get('/games', (req, res) => {
+  return res.json([]);
 })
 
+app.post('/ads', (req, res) => {
+  return res.status(201).json([]);
+})
+
+app.get('/games/:id/ads', (req, res) => {
+return res.send('concluido')
+})
+ 
 app.listen(3333);
